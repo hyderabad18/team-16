447e2e0b4e89ad42df309d0692e6f167dbea1d18
MeghanaMurala <36921264+MeghanaMurala@users.noreply.github.com>
2018-07-14 22:57:09 +0530

    Add files via upload

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'registration');
+$db = mysqli_connect('localhost', 'root', '', 'youthforseva');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
@@ -16,19 +16,25 @@ if (isset($_POST['reg_user'])) {
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $phno = mysqli_real_escape_string($db, $_POST['phno']);
+  $pin = mysqli_real_escape_string($db, $_POST['pin']);
+  $week = mysqli_real_escape_string($db, $_POST['week']);
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if (empty($phno)) { array_push($errors, "Phone number is required"); }
+  if (empty($pin)) { array_push($errors, "Pin Code is required"); }
+  if (empty($week)) { array_push($errors, "Select your preferred time"); }
   if ($password_1 != $password_2) {
 	array_push($errors, "The two passwords do not match");
   }
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM registration WHERE Name='$username' OR Emailid='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
@@ -40,14 +46,17 @@ if (isset($_POST['reg_user'])) {
     if ($user['email'] === $email) {
       array_push($errors, "email already exists");
     }
+	if ($user['phno'] === $phno) {
+      array_push($errors, "Phone Number already exists");
+    }
   }
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO users (username, email, password) 
-  			  VALUES('$username', '$email', '$password')";
+  	$query = "INSERT INTO registration (Name, Emailid, Password, Phonenumber, Preferredtime, Pincode) 
+  			  VALUES('$username', '$email', '$password', '$phno', '$week', '$pin')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
@@ -55,39 +64,31 @@ if (isset($_POST['reg_user'])) {
   }
 }
 
+// ... 
 // LOGIN USER
 if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
   if (empty($username)) {
-  	array_push($errors, "Username is required");
+    array_push($errors, "Username is required");
   }
   if (empty($password)) {
-  	array_push($errors, "Password is required");
+    array_push($errors, "Password is required");
   }
 
   if (count($errors) == 0) {
-  	$password = md5($password);
-  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-  	$results = mysqli_query($db, $query);
-  	if (mysqli_num_rows($results) == 1) {
-  	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: index.php');
-  	}else {
-  		array_push($errors, "Wrong username/password combination");
-		//header('location:login.php');
-  	}
+    $password = md5($password);
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $results = mysqli_query($db, $query);
+    if (mysqli_num_rows($results) == 1) {
+      $_SESSION['username'] = $username;
+      $_SESSION['success'] = "You are now logged in";
+      header('location: index.php');
+    }else {
+      array_push($errors, "Wrong username/password combination");
+    }
   }
-}
-
-//logout
-if(isset($_GET['logout']))
-{
-	session_destroy();
-	unset($_SESSION['username']);
-	header('location: login.php');
 }
 
 ?>
