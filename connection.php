222d28e6ee8b98cf9fe343c342c5051c599e2663
saivikranth99 <saivikranth002@gmail.com>
2018-07-14 21:10:10 +0530

    added mysql to json conversion

--- connection.php
+++ connection.php
@@ -5,18 +5,11 @@ session_start();
 //$errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'youthforseva');
+$db = mysqli_connect('localhost', 'root', '','youthforseva');
 if($db)
-	echo "connected";
-// REGISTER USER
-if (isset($_POST[''])) {
-  // receive all input values from the form
-  $name = mysqli_real_escape_string($db, $_POST['Name']);
-  $email = mysqli_real_escape_string($db, $_POST['Emailid']);
-  $password = mysqli_real_escape_string($db, $_POST['Password']);
-  $phonenumber = mysqli_real_escape_string($db, $_POST['Phonenumber']);
-  $ptime = mysqli_real_escape_string($db, $_POST['	Preferredtime']);
-  $pincode = mysqli_real_escape_string($db, $_POST['Pincode']);
+{echo "connected";
+
 	
 }
+
 ?>
