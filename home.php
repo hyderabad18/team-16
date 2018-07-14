ace3d4ca83561ca90e31a74decf90c8014cb25c7
MeghanaMurala <meghanamurala@gmail.com>
2018-07-14 16:41:44 +0530

    home page with related style sheet and images

---
+++ home.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>YFS</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="styles.css" rel="stylesheet" />
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid" style="color=red">
+    <div class="navbar-header">
+      <a class="navbar-brand" class="col-md-1 offset-2" img-responsive><img src="logo.jpg" alt="logo" width="90" height="50" border="2"></a>
+    </div>
+    <ul class="nav navbar-nav" style="height=200; float:right; font-color: white">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="slide3.jpeg" alt="Youth For Seva" style="height=500px; width=auto;">
+    </div>
+
+    <div class="item">
+      <img src="slide2.jpg" alt="Youth For Seva" style="height=500px; width=auto;">
+    </div>
+
+    <div class="item">
+      <img src="slide1.png" alt="Youth For Seva" style="height=500px; width=auto;">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="row">
+<div class="col-xs-6">
+<div class="customdivblock1">
+<img src="slide1.png" alt="logo" width="400px" height="400px" border="2">	<br><br>
+</div>
+</div>
+<div>
+	<div class="col-xs-6">
+<div class="customdivblock2">
+<br>	
+<p>Youth for Seva (YFS), founded in April 2007, is a nation-wide volunteering movement that inspires youth to volunteer, and provides them with meaningful opportunities to serve the community.
+
+<br>
+YFS aims to create positive change agents in the society, through the culture of volunteering.
+
+<b>Vision:</b>
+<br><br>
+Self-reliant communities powered by selfless individuals.
+<br><br>
+<b>Mission:</b>
+<br><br>
+To facilitate a movement of volunteering.<br>
+To empower and enable individuals to become positive change agents.<br>
+To enable institutions to effectively engage community to deliver services.<br>
+To promote sustainable lifestyles.<br>
+</p>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="footer">
+  <p><b>Registered Office</b><br>
+Youth For Seva
+‘Jnanagiri’, # 75/76, 4th Cross, 2nd Main, Soudamini Layout,
+Konanakunte, Bengaluru - 560062
+Email : contact@youthforseva.org</p>
+</div>
+</body>
+</html>
