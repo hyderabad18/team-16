a8484640bce514328ec599aa83dcc3e19640c0d6
saivikranth99 <saivikranth002@gmail.com>
2018-07-15 07:15:43 +0530

    modified  event_volmap.php

--- event_volmap.php
+++ event_volmap.php
@@ -1,18 +1,12 @@
 <?php 
 include('connection.php')
-$conn=mysqli_select_db($db,'event_vol_map');
-$confirm="CONFIRM";
-$queue="QUEUE";
-$rejected="REJECTED";
+echo $_GET['q','id'];  
+
 if (isset($_POST[''])) {
   // receive all input values from the form
-  $vid = mysqli_real_escape_string($db, $_POST['Vid']);
-  $checkin = mysqli_real_escape_string($db, $_POST['Checkin']);
-  $checkout = mysqli_real_escape_string($db, $_POST['Checkout']);
+  $vid = mysqli_real_escape_string($db, $_POST['Phonenumber']);
   $eid= mysqli_real_escape_string($db, $_POST['Eid']);
-  $eventcompleted= mysqli_real_escape_string($db, $_POST['Eventcompleted']);
-  $status= mysqli_real_escape_string($db, $_POST['status']);
-  $tid= mysqli_real_escape_string($db, $_POST['tid']);
+  $tid= mysqli_real_escape_string($db, $_POST['Tid']);
   mysqli_query($db,$query);
   //inserting in to datbase
   	$query = "INSERT INTO event_vol_map
@@ -20,7 +14,13 @@ if (isset($_POST[''])) {
 	mysqli_query($db,$query);
 //selecting from database
 $query1="select * from event_vol_map ";
-mysqli_query($db,$query1);
+$result=mysqli_query($db,$query1);
+$json_array=array();
+while($row=mysqli_fetch_assoc($result))
+{
+	$json_array[]=$row;
+}
+echo json_encode($json_array);
   ?>
   
   
