3f947039dee7f978faea073c27db1af929c3981b
saivikranth99 <saivikranth002@gmail.com>
2018-07-15 09:05:21 +0530

    changingjson.php

--- changingtojson.php
+++ changingtojson.php
@@ -1,7 +1,8 @@
 <?php
 include('connection.php');
-$sql="SELECT * from registration";
+$sql="SELECT * from youthforseregistration";
 $result=mysqli_query($db,$sql);
+echo $result;
 $json_array=array();
 while($row=mysqli_fetch_assoc($result))
 {
