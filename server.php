1151c033eb84d556c7d7d60382a6663997127877
MeghanaMurala <36921264+MeghanaMurala@users.noreply.github.com>
2018-07-15 05:40:48 +0530

    Add files via upload

--- server.php
+++ server.php
@@ -79,15 +79,14 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
     $password = md5($password);
-    $query = "SELECT * FROM registration WHERE Name='$username' AND Password='$password'";
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $results = mysqli_query($db, $query);
-    if (mysqli_num_rows($results)) {
+    if (mysqli_num_rows($results) == 1) {
       $_SESSION['username'] = $username;
       $_SESSION['success'] = "You are now logged in";
-      header('location: home.php');
+      header('location: index.php');
     }else {
       array_push($errors, "Wrong username/password combination");
-	  header('location: login.php');
     }
   }
 }
